fbf2d379cfe1d9368ff6b411c9b03b2dee0b15aa
anish <anish.kanchanapally@gmail.com>
2025-09-06 23:40:58 -0400

    updated tblasn

--- find_homologs.sh
+++ find_homologs.sh
@@ -13,7 +13,7 @@ output_file="$3"
 
 #blastn, $1 will be pident, $2 will be length, $3 will be qlen
 #we want $1 (pident) to be 100 and for $2(length) to equal $3(qlen), did some research on how to use two conditions with awk via stackoverflow: Awk AND operator
-tblastn -query "$query_file" -subject "$subject_file" -task blastn-short -outfmt "6 pident length qlen" | awk '$1 > 30 && $2>($3*0.9)' > "$output_file"
+tblastn -query "$query_file" -subject "$subject_file" -outfmt "6 pident length qlen" | awk '$1 > 30 && $2>($3*0.9)' > "$output_file"
 
 #print number of  matches (will be length of output_file (wc -l for line count, not word count))
 
